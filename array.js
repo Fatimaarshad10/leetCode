1c162db0ab0f144011811d3b1b935999ce09206d
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-02-15 19:14:18 +0500

    delete array questions

--- array.js
+++ array.js
@@ -64,21 +64,6 @@
 //     return dummy.next;
 // };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // var removeDuplicates = function(nums) {
 //     let k = 1;
 //     const arr = []
@@ -126,10 +111,10 @@
 
 // searchInsert([1, 3, 5, 6], 7);
 
-// With the binary search 
+// With the binary search
 // left side less
-// right side is higher 
-// middle and move to another target value 
+// right side is higher
+// middle and move to another target value
 
 // function searchInsert(nums, target){
 //     let left = 0
@@ -143,14 +128,13 @@
 //             left = mid + 1
 //         }
 //         else {
-//             right = mid - 1 
+//             right = mid - 1
 //         }
 //     }
 //     console.log(left)
-    
-// } 
-// searchInsert([2,3,4] , 1)
 
+// }
+// searchInsert([2,3,4] , 1)
 
 //  December 27 ===> Question no 1 ==> Easy Array
 // var plusOne = function(digits) {
@@ -168,15 +152,12 @@
 
 // plusOne([9,9])
 
-
-
-
 // var plusOne = function(digits) {
 //     let carry = 1;
 //     for (let i = 0; i < digits.length; i++) {
 //         const sum = digits[i] + carry;
-//         digits[i] = sum % 10; 
-//         carry = Math.floor(sum / 10); 
+//         digits[i] = sum % 10;
+//         carry = Math.floor(sum / 10);
 //         if (carry === 0) {
 //             break;
 //         }
@@ -193,24 +174,22 @@
 // const result = plusOne([9, 9]);
 // console.log(result);
 
-
-// Question no 2 ===> Merge array 
+// Question no 2 ===> Merge array
 // var merge = function(nums1, m, nums2, n) {
-//     nums1.length = m 
+//     nums1.length = m
 
 //     for(let i = 0 ; i < m+n ; i++){
 //         if(nums2[i] !== undefined){
 //             nums1.push(nums2[i])
-          
-//         } 
-//         m = m+n 
+
+//         }
+//         m = m+n
 //         n = 0
 //     }
-//    console.log(nums1.sort((a,b)=> a-b)) 
+//    console.log(nums1.sort((a,b)=> a-b))
 // };
 
 // merge([-10,-4,-3  ,1,2,3,0,0] , 6 , [-3,2,5,6] , 3)
-
 
 // ========= Binary Search Tree  ==========
 // function TreeNode(val, left, right) {
@@ -219,57 +198,147 @@
 //     this.right = (right===undefined ? null : right)
 //      }
 
-
 //      var sortedArrayToBST = function(nums) {
 //         if (nums.length === 0) {
 //             return null;
 //         }
-    
+
 //         return sortedArrayToBSTHelper(nums, 0, nums.length - 1);
 //     };
-    
+
 //     function sortedArrayToBSTHelper(nums, start, end) {
 //         if (start > end) {
 //             return null;
 //         }
-    
+
 //         const mid = Math.floor((start + end) / 2);
 //         const val = nums[mid];
 //         const node = new TreeNode(val);
-    
+
 //         node.left = sortedArrayToBSTHelper(nums, start, mid - 1);
 //         node.right = sortedArrayToBSTHelper(nums, mid + 1, end);
-    
+
 //         return node;
 //     }
-    
+
 //     // Example usage:
 //     const root = sortedArrayToBST([-10, -3, 0, 5, 9]);
 //     console.log(root);
 
+// var generate = function(numRows) {
+//     const data = []
+//     for (let i = 0; i < numRows; i++) {
+//         const row = [];
+
+//         for (let j = 0; j <= i; j++) {
+//             if (j === 0 || j === i) {
+//                 row.push(1);
+//             } else {
+//                 // Calculate the value based on the previous row
+//                 console.log(data[2-1][2-1] + data[2-1][2])
+//                 row.push(data[i - 1][j - 1] + data[i - 1][j]);
+//             }
+
+//         }
+
+//         data.push(row);
+//         // console.log(row.join(' '));
+//     }
+
+//     // console.log(data);
+// };
+
+// generate(5)
+
+// Merge sorted array O(m+n)
+// /**
+//  * @param {number[]} nums1
+//  * @param {number} m
+//  * @param {number[]} nums2
+//  * @param {number} n
+//  * @return {void} Do not return anything, modify nums1 in-place instead.
+//  */
+// var merge = function (nums1, m, nums2, n) {
+//   nums1.length = m;
+//   for (let i = 0; i <= m; i++) {
+//     for (let k = 0; k < n; k++) {
+//       if (nums2[k] !== undefined) {
+//         nums1.push(nums2[k]);
+//       }
+//     }
+//     m = m + n;
+//     n = 0;
+//   }
+//   console.log(nums1.sort((a, b) => a - b));
+// };
+
+// merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+// merge([1], 1, [], 0);
+// merge([0], 0, [1], 1);
 
 
-var generate = function(numRows) {
-    const data = []
-    for (let i = 0; i < numRows; i++) {
-        const row = [];
+// 15/02/2024
+// Deleting From the End of an Array
+// const newArray = new Array(10)
+// let value = 0
+// for (let i = 0; i < 6; i++) {
+//     newArray[value] = i
+//     value++
+// }
+// value--
+// for (let i = 0; i < value; i++) {
+//     console.log("Index " +i +" contains " +newArray[i])
+    
+// }
 
-        for (let j = 0; j <= i; j++) {
-            if (j === 0 || j === i) {
-                row.push(1);
-            } else {
-                // Calculate the value based on the previous row
-                console.log(data[2-1][2-1] + data[2-1][2])
-                row.push(data[i - 1][j - 1] + data[i - 1][j]);
-            }
+// // Deleting From the Start of an Array
+// for (let i = 0; i < value; i++) {
+//    newArray[i-1] = newArray[i]
+    
+// }
+// value--;
+// for (let i = 0; i < value; i++) {
+//     console.log("Index " +i +" contains " +newArray[i])
+// }
 
-        }
+// // Deleting From Anywhere in the Array
 
-        data.push(row);
-        // console.log(row.join(' '));
-    }
+// for (let i = 2; i < value; i++) {
+//     newArray[i-1] = newArray[i]
+// }
+// value--
+// for (let i = 0; i < value; i++) {
+//     console.log("Index " +i +" contains " +newArray[i])
+// }
 
-    // console.log(data);
-};
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+//  */
+// var removeElement = function(nums, val) {
+//     var k = 0
+//    for(i = 0; i < nums.length ; i++){
+//     if(nums[i] !== val){
+//         nums[k] = nums[i];
+//         k++;
+//     }
+//    }
+//    console.log(nums)  
+// };
+// removeElement([2,3,4] ,  3)
+// removeElement([0,1,2,2,3,0,4,2] ,  2)
 
-generate(5)
+
+// const removeDuplicate = function (nums){
+//     let k = 0
+//     for(let i = 0 ; i < nums.length ; i++){
+//         if(nums[i] !== nums[i+1]){
+//             nums[k] = nums[i]
+//             k++
+//         }
+//     }
+//     console.log(k)
+
+// }
+// removeDuplicate([1,1,2])
